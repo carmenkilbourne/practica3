7ab58f0f6ccd34820f1abf69806d1c6b9adc2a08
carmenkilbourne <113438693+carmenkilbourne@users.noreply.github.com>
2024-11-21 17:42:52 +0100

    falta en el post ninos comprobar que la ubicacion existe

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import { MongoClient, ObjectId } from 'mongodb'
 import { LugarModel, NinosModel } from "./types.ts";
-import { ModelToNino } from "./resolvers.ts";
+import { ModelToLugar, ModelToNino,haversine } from "./resolvers.ts";
 
 // Connection URL
 const url = Deno.env.get("MONGO_URL");
@@ -28,8 +28,8 @@ const dbName = 'nebrijadb';
             const lugar = await lugarcollection.findOne({nombre});
             const coordenadas =  body.coordenadas;
             if(!lugar){
-              if(coordenadas.length === 2 && coordenadas.every(c => typeof c === "number")){//error aqui
-                const {insertedId } =  await lugarcollection.insertOne({coordenadas: body.coordenadas, nombre: body.nombre, ninosBuenos: body.ninosBuenos });
+              if(coordenadas.length === 2 && coordenadas.every(c => typeof c === "number")){
+                const {insertedId } =  await lugarcollection.insertOne({coordenadas: body.coordenadas, nombre: body.nombre, ninosBuenos: body.ninosBuenos});
               return new Response(JSON.stringify({coordenadas:body.coordenadas,nombre:body.nombre,ninosBuenos:body.ninosBuenos,id:insertedId }),{status:201});
               }   
               else{
@@ -41,16 +41,17 @@ const dbName = 'nebrijadb';
             }
 
         }
-        return new Response("Data body not found",{status:400})
+        return new Response("os campos nombre, coordenadas y numero de ninos buenos son obligatorios",{status:400})
       }
-      else if(path === "/ninos"){
+      else if(path === "/ninos"){ //comprobar que la ubicacion existe 
         const ninos = await req.json();
-        if(ninos.nombre ){
+        if(ninos.nombre && ninos.comportamiento && ninos.ubicacion){
           const nombre =  ninos.nombre;
           const nino = await ninoscollection.findOne({nombre});
           const comportamiento = ninos.comportamiento;
+          
           if(!nino){
-            if(typeof comportamiento === "boolean" ){//error aqui
+            if(typeof comportamiento === "boolean" ){
               const {insertedId } =  await ninoscollection.insertOne({ nombre: ninos.nombre, comportamiento:ninos.comportamiento, ubicacion:ninos.ubicacion});
             return new Response(JSON.stringify({nombre: ninos.nombre, comportamiento:ninos.comportamiento, ubicacion:ninos.ubicacion,id:insertedId }),{status:201});
             }   
@@ -60,7 +61,7 @@ const dbName = 'nebrijadb';
             return new Response("El nino ya existe",{status:409})
           }
         }
-        return new Response("Data body not found",{status:400})
+        return new Response("Los campos nombre, comportamiento y ubicacion de ninos son obligatorios",{status:400})
       }
     }
     else if(method === "GET"){
@@ -70,18 +71,40 @@ const dbName = 'nebrijadb';
         ninosMalossdb.map((n) =>ModelToNino(lugarcollection,n)));
         return new Response(JSON.stringify(ninosMalos),{status:200});
       }
-      if(path === "/ninos/buenos"){ //hay que hacer condicion por si no existe ningun true
+      else if(path === "/ninos/buenos"){ //hay que hacer condicion por si no existe ningun true
         const ninosBuenosdb = await ninoscollection.find({comportamiento:true}).toArray();
+        
         const ninosBuenos = await Promise.all(
         ninosBuenosdb.map((n) =>ModelToNino(lugarcollection,n)));
         return new Response(JSON.stringify(ninosBuenos),{status:200});
       }
-    /*   if(path === "/entregas"){
-        const ninosBuenosdb = await ninoscollection.find().toArray();
+      else  if(path === "/entregas"){
+        const ubicacionesdb = await lugarcollection.find().toArray();
+        const ubicaciones = await Promise.all(ubicacionesdb.map((u) =>ModelToLugar(u)));
 
-      } */
+        const ubicacionesOrdenadas = ubicaciones.sort((a, b) => {
+          return b.ninosBuenos - a.ninosBuenos;
+        });
+        return new Response(JSON.stringify(ubicacionesOrdenadas),{status:200});
+
+      } 
+      else  if(path === "/ruta"){
+        const ubicacionesdb = await lugarcollection.find().toArray();
+        const ubicaciones = await Promise.all(ubicacionesdb.map((u) =>ModelToLugar(u)));
+
+        const ubicacionesOrdenadas = ubicaciones.sort((a, b) => {
+          return b.ninosBuenos - a.ninosBuenos;
+        });
+        const distancias = ubicacionesOrdenadas.reduce((acc,elem,i,a) => {
+          if (i === a.length - 1) return acc;
+      const siguiente = a[i + 1];
+          return acc + haversine(elem.coordenadas[0],elem.coordenadas[1],siguiente.coordenadas[0],siguiente.coordenadas[1]);
+        },0);
+        return new Response("distancia total a recorrer por Santa Claus= "+distancias,{status:200});
+      }
     }
     return new Response("No endpoint",{status:404});
   } 
   Deno.serve({ port: 6768 }, handler);
+
 
--- resolvers.ts
+++ resolvers.ts
@@ -13,10 +13,7 @@ export const  ModelToNino = async(
         id:ninosdb._id!.toString(),
         ubicacion:lugares.map((l) => ModelToLugar(l))
     };
-}
-
-
-
+};
 
 export const  ModelToLugar = (lugarmodel:LugarModel):Lugar =>({
     id:lugarmodel._id!.toString(),
@@ -24,3 +21,17 @@ export const  ModelToLugar = (lugarmodel:LugarModel):Lugar =>({
     coordenadas:lugarmodel.coordenadas,
     ninosBuenos:lugarmodel.ninosBuenos
 });
+export const haversine = (lat1: number, lon1: number, lat2: number, lon2: number) => {
+    const R = 6371; // Radio de la Tierra en km
+    const toRad = (deg: number) => (deg * Math.PI) / 180; // Conversión a radianes
+    const dLat = toRad(lat2 - lat1);
+    const dLon = toRad(lon2 - lon1);
+    const lat1Rad = toRad(lat1);
+    const lat2Rad = toRad(lat2);
+    const a = Math.sin(dLat / 2) ** 2 +
+         Math.cos(lat1Rad) * Math.cos(lat2Rad) *
+         Math.sin(dLon / 2) ** 2;
+  
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return R * c; // Distancia en km
+  }
